2a2585f175e8736ca079af600da4d1d77763476b
kstrotz <kevin.j.strotz@gmail.com>
2016-09-07 09:30:24 -0400

    Update to install URServer.py with toolbox

--- installURToolbox.m
+++ installURToolbox.m
@@ -162,6 +162,80 @@ for i = 1:n
 end
 set(wb,'Visible','off');
 
+%% Migrate toolbox support folder
+
+toolboxContent = 'URToolboxSupport';
+if ~isdir(toolboxContent)
+    error(sprintf(...
+        ['Change your working directory to the location of "installURToolbox.m".\n',...
+         '\n',...
+         'If this problem persists:\n',...
+         '\t(1) Unzip your original download of "URToolbox" into a new directory\n',...
+         '\t(2) Open a new instance of MATLAB "as administrator"\n',...
+         '\t\t(a) Locate MATLAB shortcut\n',...
+         '\t\t(b) Right click\n',...
+         '\t\t(c) Select "Run as administrator"\n',...
+         '\t(3) Change your "working directory" to the location of "installURToolbox.m"\n',...
+         '\t(4) Enter "installURToolbox" (without quotes) into the command window\n',...
+         '\t(5) Press Enter.']));
+end
+files = dir(toolboxContent);
+wb = waitbar(0,'Copying UR Toolbox toolbox contents...');
+n = numel(files);
+fprintf('Copying UR Toolbox contents:\n');
+for i = 1:n
+    % source file location
+    source = fullfile(toolboxContent,files(i).name);
+    % destination location
+    destination = toolboxRoot;
+    if files(i).isdir
+        switch files(i).name
+            case '.'
+                %Ignore
+            case '..'
+                %Ignore
+            otherwise
+                fprintf('\t%s...',files(i).name);
+                nDestination = fullfile(destination,files(i).name);
+                [isDir,msg,msgID] = mkdir(nDestination);
+                if isDir
+                    [isCopy,msg,msgID] = copyfile(source,nDestination,'f');
+                    if isCopy
+                        fprintf('[Complete]\n');
+                    else
+                        bin = msg == char(10);
+                        msg(bin) = [];
+                        bin = msg == char(13);
+                        msg(bin) = [];
+                        fprintf('[Failed: "%s"]\n',msg);
+                    end
+                else
+                    bin = msg == char(10);
+                    msg(bin) = [];
+                    bin = msg == char(13);
+                    msg(bin) = [];
+                    fprintf('[Failed: "%s"]\n',msg);
+                end
+        end
+    else
+        fprintf('\t%s...',files(i).name);
+        [isCopy,msg,msgID] = copyfile(source,destination,'f');
+        
+        if isCopy == 1
+            fprintf('[Complete]\n');
+        else
+            bin = msg == char(10);
+            msg(bin) = [];
+            bin = msg == char(13);
+            msg(bin) = [];
+            fprintf('[Failed: "%s"]\n',msg);
+        end
+    end
+    waitbar(i/n,wb);
+end
+set(wb,'Visible','off');
+
+
 %% Save toolbox path
 %addpath(genpath(toolboxRoot),'-end');
 addpath(toolboxRoot,'-end');
